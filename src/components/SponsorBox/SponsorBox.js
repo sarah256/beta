e10b75d14aad98adf8d01c5610f51d66c8a3cf4c
Warren-Partridge <maps@perdix.org>
2018-06-28 21:20:12 -0400

    Add babel-eslint so we can write es6 event handlers

--- src/components/SponsorBox/SponsorBox.js
+++ src/components/SponsorBox/SponsorBox.js
@@ -1,16 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Header from "../Header";
 
-const SponsorBox = () => (
-  <Fragment>
-    <Header
-      contentProp="Sponsor"
-      colorProp="#dc4b6a"
-      backgroundProp="#FFFFFF"
-    />
-    <p>We would love to have you on board. Contact us here!</p>
-    <button>Contact Us</button>
-  </Fragment>
-);
+class SponsorBox extends Component {
+  handleSponsorButtonClick = e => console.log("Sponsor button clicked!");
+
+  render() {
+    return (
+      <Fragment>
+        <Header
+          contentProp="Sponsor"
+          colorProp="#dc4b6a"
+          backgroundProp="#FFFFFF"
+        />
+        <p>We would love to have you on board. Contact us here!</p>
+        <button onClick={this.handleSponsorButtonClick}>Contact Us</button>
+      </Fragment>
+    );
+  }
+}
 
 export default SponsorBox;
