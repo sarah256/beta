eede739b5095c83d2e18395c97415c4795ff5a01
nampham148 <phamquangnam148@gmail.com>
2018-07-20 15:24:19 +0700

    add image for event schedule

--- src/components/EventSchedule/EventSchedule.js
+++ src/components/EventSchedule/EventSchedule.js
@@ -3,6 +3,9 @@ import { ScheduleTable } from "../";
 import styled from "styled-components";
 import { Flex, Box } from "grid-styled";
 
+import gridGreen1 from "./assets/dot grid green 1.svg";
+import circleBlue1 from "./assets/Thin circle blue 1.svg";
+
 const eventsSat = [
   { time: "9:30 AM", name: "registration" },
   { time: "11:30 AM", name: "opening ceremony" },
@@ -34,14 +37,28 @@ class EventSchedule extends Component {
   render() {
     return (
       <React.Fragment>
-        <EventScheduleTitle> Event Schedule </EventScheduleTitle>
-        <Flex flexWrap="wrap" justifyContent="space-between">
-          <Box px={2} width={[1, 1, 1 / 2]}>
-            <ScheduleTable events={eventsSat} title="Saturday" />
+        <Flex>
+          <Flex
+            width={[0, 1 / 20]}
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <img src={circleBlue1} />
+          </Flex>
+          <Box width={[1, 85 / 100]}>
+            <EventScheduleTitle> Event Schedule </EventScheduleTitle>
+            <Flex flexWrap="wrap" justifyContent="space-between">
+              <Box px={2} width={[1, 1, 1 / 2]}>
+                <ScheduleTable events={eventsSat} title="Saturday" />
+              </Box>
+              <Box px={2} width={[1, 1, 1 / 2]}>
+                <ScheduleTable events={eventsSun} title="Sunday" />
+              </Box>
+            </Flex>
           </Box>
-          <Box px={2} width={[1, 1, 1 / 2]}>
-            <ScheduleTable events={eventsSun} title="Sunday" />
-          </Box>
+          <Flex width={[0, 1 / 10]} alignItems="center" justifyContent="center">
+            <img src={gridGreen1} />
+          </Flex>
         </Flex>
       </React.Fragment>
     );
