f1f06744531657e09c855451d718bf47e3c1109b
Warren-Partridge <maps@perdix.org>
2018-07-27 20:45:06 -0400

    Add grid styling for attend+sponsor, add Roboto Slab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Flex, Box } from "grid-styled";
 import {
   AttendBox,
   SponsorBox,
@@ -12,55 +13,64 @@ import HeroText from "./components/HeroText";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="Flair">
-          <div className="headerbar">
-            <HeroText
-              title="Boston"
-              titleBold="Hacks"
-              date="November 10th - November 11th"
-              location="Boston University"
-            />
+      <div>
+        <div className="App">
+          <div className="Flair">
+            <div className="headerbar">
+              <HeroText
+                title="Boston"
+                titleBold="Hacks"
+                date="November 10th - November 11th"
+                location="Boston University"
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <AttendBox />
-          <SponsorBox />
-        </div>
-        <div>
-          <h2>
-            <Header
-              contentProp="Event Schedule"
-              colorProp="#EF833F"
-              backgroundProp="#FFFFFF"
-            />
-          </h2>
-          <EventSchedule />
-        </div>
+          <Flex className="attendSponsorFlex">
+            <Box width={[0, 1 / 4, 1 / 4]} />
+            <Box width={[1, 1 / 4, 1 / 5]}>
+              <AttendBox />
+            </Box>
+            <Box width={[0, 0, 1 / 10]} />
+            <Box width={[1, 1 / 4, 1 / 5]}>
+              <SponsorBox />
+            </Box>
+            <Box width={[0, 1 / 4, 1 / 4]} />
+          </Flex>
+          <div>
+            <h2>
+              <Header
+                contentProp="Event Schedule"
+                colorProp="#EF833F"
+                backgroundProp="#FFFFFF"
+              />
+            </h2>
+            <EventSchedule />
+          </div>
 
-        <div>
-          <TracksAndWorkshops />
-        </div>
+          <div>
+            <TracksAndWorkshops />
+          </div>
 
-        <div>
-          <h2>
-            <Header
-              contentProp="Frequently Asked Questions"
-              colorProp="#3CBFCE"
-              backgroundProp="#FFFFFF"
-            />
-          </h2>
-          <FAQSection />
-        </div>
+          <div>
+            <h2>
+              <Header
+                contentProp="Frequently Asked Questions"
+                colorProp="#3CBFCE"
+                backgroundProp="#FFFFFF"
+              />
+            </h2>
+            <FAQSection />
+          </div>
 
-        <div>
-          <h2>
-            <Header
-              contentProp="The Footer"
-              colorProp="#FFFFFF"
-              backgroundProp="#3dbecd"
-            />
-          </h2>
+          <div>
+            <h2>
+              <Header
+                contentProp="The Footer"
+                colorProp="#FFFFFF"
+                backgroundProp="#3dbecd"
+              />
+            </h2>
+          </div>
         </div>
       </div>
     );
